8098db7b87acf0481c949f84cfebb411f9701dea
Dan Rudolf <dan.rudolf@brandingbrand.com>
2023-03-06 19:44:42 -0600

    Initial Commit

---
+++ TestNativeSDK/TestNativeSDK.h
@@ -0,0 +1,18 @@
+//
+//  TestNativeSDK.h
+//  TestNativeSDK
+//
+//  Created by Branding Brand on 3/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestNativeSDK.
+FOUNDATION_EXPORT double TestNativeSDKVersionNumber;
+
+//! Project version string for TestNativeSDK.
+FOUNDATION_EXPORT const unsigned char TestNativeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestNativeSDK/PublicHeader.h>
+
+
